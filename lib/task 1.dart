a31eefeee24f6e8313f0801fff6b49ef0e37ae4d
elgato7-y <usifka@gmail.com>
2025-07-23 20:17:19 +0300

    Delete lib/task 1.dart

--- lib/task 1.dart
+++
@@ -1,9 +0,0 @@
-//given 2 numbers n and m. print the summation of their last digits
-import 'dart:io';
-void main() {
-int num1 = int.parse(stdin.readLineSync()!);
-int num2 = int.parse(stdin.readLineSync()!);
-int sum = ( num1 % 10 ) + ( num2 % 10 ) ;
-print ("your answer is : $sum ");
-}
-
