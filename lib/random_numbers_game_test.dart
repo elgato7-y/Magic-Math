8ff9e48cd8aa84929416db68534bfe3146c1ff5b
elgato7-y <usifka@gmail.com>
2025-07-23 20:14:43 +0300

    random_numbers_game_test.dart

--- lib/random_numbers_game_test.dart
+++ lib/random_numbers_game_test.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:math';
 void main() {
+  int correctCounter = 0  , wrongCounter = 0 ;
   Random random = Random();
   bool again = true;
   int num1, num2, sum;
@@ -12,10 +13,12 @@ void main() {
     sum = int.parse(stdin.readLineSync()!);
     if (sum == num1 + num2) {
       print ("correct answer");
+      correctCounter++;
     }
     else {
       print("wrong answer");
       print ("the correct answer = ${num1 + num2}");
+      wrongCounter++ ;
     }
     print("do you want to continue? [YES/NO]");
     String ans = stdin.readLineSync()!;
@@ -27,27 +30,6 @@ void main() {
       again = false;
     }
     }
+    print("your total correct answers is $correctCounter");
+  print ("your total wrong answers is $wrongCounter");
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
